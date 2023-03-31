3c0370bec347d92815c4daa6fb82efcd94c45b73
Restiti <rem59570@gmail.com>
2023-03-31 10:20:29 +0200

    Flou arriere plan pour win

--- AndroidStudioProjects/masyu_puzzle/lib/screen_game.dart
+++ AndroidStudioProjects/masyu_puzzle/lib/screen_game.dart
@@ -1,3 +1,5 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -11,133 +13,175 @@ class MyScreenGame extends StatefulWidget {
 class _MyScreenGame extends State<MyScreenGame> {
   @override
   Widget build(BuildContext context) {
-    var largeurWidthEcran = MediaQuery.of(context).size.width;
-    var hauteurHeightEcran = MediaQuery.of(context).size.height;
+    double largeurWidthEcran = MediaQuery.of(context).size.width;
+    double hauteurHeightEcran = MediaQuery.of(context).size.height;
+    double opaciteWin = 0;
+    bool isWin = false;
     return Scaffold(
-      backgroundColor: Color(0xffEA5455),
-      body: Column(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-        SizedBox(height: hauteurHeightEcran * 0.04),
-        Container(
-          color: Color(0xffEA5455),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              SizedBox(width: largeurWidthEcran * 0.06),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: SizedBox(
-                    width: largeurWidthEcran * 0.1,
-                    height: hauteurHeightEcran * 0.06,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        Navigator.pushNamed(
-                          context,
-                          '/',
-                        );
-                      },
-                      child: Text('Retour',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      style: ElevatedButton.styleFrom(
-                          shape: StadiumBorder(),
-                          backgroundColor: Color(0xFF002B5B)),
-                    ),
-                  ),
-                ),
-              ),
-              Text("Facile"),
-            ],
-          ),
-        ),
-        Container(
-          color: Color(0xffEA5455),
-          child:
-              Text("⏱️00:00:22", style: TextStyle(fontWeight: FontWeight.bold)),
-        ),
-        SizedBox(height: hauteurHeightEcran * 0.02),
-        SizedBox(
-          height: hauteurHeightEcran * 0.5,
-          width: largeurWidthEcran * 0.9,
-          child: ElevatedButton(
-            onPressed: () {},
-            child: Text('Ma grille',
-                style: TextStyle(fontWeight: FontWeight.bold)),
-            style: ElevatedButton.styleFrom(backgroundColor: Color(0xFF002B5B)),
-          ),
-        ),
-        SizedBox(height: hauteurHeightEcran * 0.015),
-        Column(
-          children: [
-            SizedBox(
-              width: largeurWidthEcran,
-              height: hauteurHeightEcran * 0.06,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Soumettre',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  style: ElevatedButton.styleFrom(
-                      shape: StadiumBorder(),
-                      backgroundColor: Color(0xFF002B5B)),
-                ),
-              ),
-            ),
-            SizedBox(height: hauteurHeightEcran * 0.015),
-            SizedBox(
-              width: largeurWidthEcran,
-              height: hauteurHeightEcran * 0.06,
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15.0),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  child: Text('Enregistrer',
-                      style: TextStyle(fontWeight: FontWeight.bold)),
-                  style: ElevatedButton.styleFrom(
-                      shape: StadiumBorder(),
-                      backgroundColor: Color(0xFF002B5B)),
-                ),
-              ),
-            ),
-            SizedBox(height: hauteurHeightEcran * 0.015),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+        backgroundColor: Color(0xffEA5455),
+        body: SafeArea(
+            child: Stack(
               children: [
-                SizedBox(
-                  width: largeurWidthEcran * 0.5,
-                  height: hauteurHeightEcran * 0.06,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15.0),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Reset',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      style: ElevatedButton.styleFrom(
-                          shape: StadiumBorder(),
-                          backgroundColor: Color(0xFF002B5B)),
+                Container(
+                  padding: EdgeInsets.fromLTRB(0, hauteurHeightEcran * 0.04, 0, 0),
+                  child: Column(children: [
+                    Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          SizedBox(width: largeurWidthEcran * 0.06),
+                          Expanded(
+                            child: Align(
+                              alignment: Alignment.topLeft,
+                              child: SizedBox(
+                                width: largeurWidthEcran * 0.175,
+                                height: hauteurHeightEcran * 0.06,
+                                child: ElevatedButton(
+                                  onPressed: () {
+                                    Navigator.pushNamed(
+                                      context,
+                                      '/',
+                                    );
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                      shape: const StadiumBorder(),
+                                      backgroundColor: Color(0xFF002B5B)),
+                                  child: const Text('Retour',
+                                      style: TextStyle(
+                                          fontFamily: 'Langar', fontSize: 18)),
+                                ),
+                              ),
+                            ),
+                          ),
+                          const Text("Facile",
+                              style: TextStyle(fontFamily: 'Langar', fontSize: 28)),
+                        ]),
+                    Container(
+                      color: const Color(0xffEA5455),
+                      child: const Text("⏱️00:00:22",
+                          style: TextStyle(fontFamily: 'Langar', fontSize: 28)),
                     ),
-                  ),
+                    SizedBox(height: hauteurHeightEcran * 0.02),
+                    SizedBox(
+                      height: hauteurHeightEcran * 0.55,
+                      width: largeurWidthEcran * 0.9,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Color(0xFF002B5B)),
+                        child: const Text('Ma grille',
+                            style: TextStyle(fontFamily: 'Langar', fontSize: 28)),
+                      ),
+                    ),
+                    SizedBox(height: hauteurHeightEcran * 0.03),
+                    Column(
+                      children: [
+                        SizedBox(
+                          width: largeurWidthEcran,
+                          height: hauteurHeightEcran * 0.06,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: ElevatedButton(
+                              onPressed: () {
+                                opaciteWin = 1;
+                                isWin = true;
+                                print("C'est gagné! ");
+                                print("Opacité:  " + opaciteWin.toString());
+                                print("isWin: " + isWin.toString());
+                              },
+                              child: Text('Soumettre',
+                                  style: TextStyle(
+                                      fontFamily: 'Langar', fontSize: 28)),
+                              style: ElevatedButton.styleFrom(
+                                  shape: StadiumBorder(),
+                                  backgroundColor: Color(0xFF002B5B)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: hauteurHeightEcran * 0.02),
+                        SizedBox(
+                          width: largeurWidthEcran,
+                          height: hauteurHeightEcran * 0.06,
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(15.0),
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  shape: const StadiumBorder(),
+                                  backgroundColor: const Color(0xFF002B5B)),
+                              child: const Text('Enregistrer',
+                                  style: TextStyle(
+                                      fontFamily: 'Langar', fontSize: 28)),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: hauteurHeightEcran * 0.02),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              width: largeurWidthEcran * 0.5,
+                              height: hauteurHeightEcran * 0.06,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15.0),
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  style: ElevatedButton.styleFrom(
+                                      shape: const StadiumBorder(),
+                                      backgroundColor: Color(0xFF002B5B)),
+                                  child: const Text('Reset',
+                                      style: TextStyle(
+                                          fontFamily: 'Langar', fontSize: 28)),
+                                ),
+                              ),
+                            ),
+                            SizedBox(
+                              width: largeurWidthEcran * 0.5,
+                              height: hauteurHeightEcran * 0.06,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15.0),
+                                child: ElevatedButton(
+                                  onPressed: () {},
+                                  child: Text('Aide',
+                                      style: TextStyle(
+                                          fontFamily: 'Langar', fontSize: 28)),
+                                  style: ElevatedButton.styleFrom(
+                                      shape: StadiumBorder(),
+                                      backgroundColor: Color(0xFF002B5B)),
+                                ),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  ]),
                 ),
-                SizedBox(
-                  width: largeurWidthEcran * 0.5,
-                  height: hauteurHeightEcran * 0.06,
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(15.0),
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      child: Text('Aide',
-                          style: TextStyle(fontWeight: FontWeight.bold)),
-                      style: ElevatedButton.styleFrom(
-                          shape: StadiumBorder(),
-                          backgroundColor: Color(0xFF002B5B)),
-                    ),
+                if(isWin) BackdropFilter(
+                  filter: ImageFilter.blur(sigmaX: 4, sigmaY: 4),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Align(
+                        alignment: Alignment.center,
+                        child: Text("C'est gagné",
+                            style: TextStyle(
+                                fontFamily: 'Langar',
+                                fontSize: 28,
+                                color: Colors.white)),
+                      ),
+                      SizedBox(
+                        width: largeurWidthEcran * 0.4,
+                      ),
+                      Align(
+                          child: ElevatedButton(
+                            onPressed: () {},
+                            child: const Text('ACCEUIL',
+                                style: TextStyle(fontFamily: 'Langar', fontSize: 28)),
+                          ))
+                    ],
                   ),
                 ),
               ],
-            )
-          ],
-        )
-      ]),
-    );
+            )));
   }
 }
