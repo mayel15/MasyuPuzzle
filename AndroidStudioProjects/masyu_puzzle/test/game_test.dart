7d8626dc574a2d504cf11bef1cb14b6039de63c1
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-04-11 14:49:07 +0200

    clean code

--- AndroidStudioProjects/masyu_puzzle/test/game_test.dart
+++ AndroidStudioProjects/masyu_puzzle/test/game_test.dart
@@ -1,17 +1,8 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:masyu_puzzle/enum.dart';
 import 'package:masyu_puzzle/cell.dart';
 import 'package:masyu_puzzle/game.dart';
-import 'package:masyu_puzzle/line.dart';
-
 void main() {
   /* unit tests for game */
   group('Game tests', () {
