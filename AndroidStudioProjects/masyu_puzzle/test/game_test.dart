e99a2025a953ce467fa27c0ed1b0ba683b484265
Pape Mayel Diagne THIAM <97792012+mayel15@users.noreply.github.com>
2023-04-11 13:53:21 +0200

    unit tests for Game and some of them weren't passed

--- AndroidStudioProjects/masyu_puzzle/test/game_test.dart
+++ AndroidStudioProjects/masyu_puzzle/test/game_test.dart
@@ -14,5 +14,29 @@ import 'package:masyu_puzzle/line.dart';
 
 void main() {
   /* unit tests for game */
-  
+  group('Game tests', () {
+    test('linkedCells should add the correct number of lines to the game', () {
+      final game = Game();
+      game.linkedCells();
+      final gridSize = game.grid.listCells.length;
+      final expectedLines = gridSize * gridSize * 4 - gridSize * 4;
+      expect(game.lines.length, equals(expectedLines));
+    });
+
+    test('checkNbvoisins should return the correct number of neighbors for each cell', () {
+      final game = Game();
+      game.grid.listCells = [
+        [Cell(0, 0, CellType.none), Cell(0, 1, CellType.none), Cell(0, 2, CellType.none)],
+        [Cell(1, 0, CellType.none), Cell(1, 1, CellType.none), Cell(1, 2, CellType.none)],
+        [Cell(2, 0, CellType.none), Cell(2, 1, CellType.none), Cell(2, 2, CellType.none)]
+      ];
+      game.linkedCells();
+      final expectedNbVoisins = [1, 2, 2, 2, 2, 2, 2, 1, 1];
+      for (int i = 0; i < game.grid.listCells.length; i++) {
+        for (int j = 0; j < game.grid.listCells.length; j++) {
+          expect(game.grid.listCells.elementAt(i).elementAt(j).getNbVoisins(), equals(expectedNbVoisins[i * 3 + j]));
+        }
+      }
+    });
+  });
 }
